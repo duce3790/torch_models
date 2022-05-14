f61711f0d4c8c610cec1819b49fed222d36af43f
cyy <cyyever@outlook.com>
2022-05-14 18:08:29 +0800

    fix code

--- transformer.py
+++ transformer.py
@@ -8,57 +8,7 @@ from torch.nn import (Dropout, Embedding, Linear, LogSoftmax, Module,
 
 
 class PositionalEncoding(Module):
-    "Implement the PE function."
-
-    def __init__(self, d_model, max_len):
-        super().__init__()
-
-        # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
-        )
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.pe = pe
-
-    def forward(self, x):
-        x = x + self.pe.to(x.device)[:, : x.size(1)]
-        return x
-
-
-class TransformerLangModel(Module):
-    def __init__(
-        self,
-        token_num: int,
-        max_len: int,
-        d_model: int = 512,
-    ):
-        super().__init__()
-        self.token_num = token_num
-        self.max_len = max_len
-        self.embedding = Embedding(token_num, embedding_dim=d_model)
-        self.positional_encoding = PositionalEncoding(d_model=d_model, max_len=max_len)
-        encoder_layers = TransformerEncoderLayer(
-            d_model=d_model, nhead=8, batch_first=True
-        )
-        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers=6)
-        self.linear = Linear(d_model, token_num)
-        self.activation = LogSoftmax()
-
-    def forward(self, src: Tensor) -> Tensor:
-        src_embedding = self.positional_encoding(self.embedding(src))
-        output = self.transformer_encoder(
-            src=src_embedding,
-        )
-        output = self.activation(self.linear(output)).view(-1, self.token_num)
-        return output
-
-
-class PositionalEncoding2(Module):
-    def __init__(self, d_model: int, max_len: int, dropout: float = 0):
+    def __init__(self, d_model: int, max_len: int, dropout: float = 0.1):
         super().__init__()
         self.dropout = Dropout(p=dropout)
 
@@ -80,6 +30,33 @@ class PositionalEncoding2(Module):
         return self.dropout(x)
 
 
+class TransformerLangModel(Module):
+    def __init__(
+        self,
+        token_num: int,
+        max_len: int,
+        d_model: int = 512,
+        nhead: int = 8,
+    ):
+        super().__init__()
+        self.token_num = token_num
+        self.max_len = max_len
+        self.embedding = Embedding(token_num, embedding_dim=d_model)
+        self.positional_encoding = PositionalEncoding(d_model=d_model, max_len=max_len)
+        encoder_layers = TransformerEncoderLayer(d_model=d_model, nhead=nhead)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers=6)
+        self.linear = Linear(d_model, token_num)
+        self.activation = LogSoftmax()
+
+    def forward(self, src: Tensor) -> Tensor:
+        src_embedding = self.positional_encoding(self.embedding(src))
+        output = self.transformer_encoder(
+            src=src_embedding,
+        )
+        output = self.activation(self.linear(output)).view(-1, self.token_num)
+        return output
+
+
 class TransformerClassificationModel(Module):
     def __init__(
         self,
@@ -94,11 +71,8 @@ class TransformerClassificationModel(Module):
         self.max_len = max_len
         self.token_num = token_num
         self.embedding = Embedding(token_num, embedding_dim=d_model)
-        self.positional_encoding = PositionalEncoding2(d_model=d_model, max_len=max_len)
-        encoder_layers = TransformerEncoderLayer(
-            d_model=d_model,
-            nhead=nhead,
-        )
+        self.positional_encoding = PositionalEncoding(d_model=d_model, max_len=max_len)
+        encoder_layers = TransformerEncoderLayer(d_model=d_model, nhead=nhead)
         self.transformer_encoder = TransformerEncoder(
             encoder_layers, num_layers=num_encoder_layer
         )
