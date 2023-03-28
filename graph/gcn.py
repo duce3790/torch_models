830d30f4bb3c03b7b825533f8b8e9c3d62f1edcc
cyy <cyyever@outlook.com>
2023-03-28 10:34:07 +0800

    fix model

---
+++ graph/gcn.py
@@ -0,0 +1,24 @@
+import torch.nn
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
+
+
+class SimpleGCN(torch.nn.Module):
+    def __init__(self, num_features, num_classes):
+        super().__init__()
+        self.conv1 = GCNConv(num_features, 1024)
+        self.conv2 = GCNConv(1024, 512)
+        self.conv3 = GCNConv(512, 128)
+        self.classifier = Linear(128, num_classes)
+
+    def forward(self, x, edge_index):
+        x = x[0]
+        edge_index = edge_index[0]
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = self.conv3(h, edge_index)
+        h = h.relu()  # Final GNN embedding space.
+        # Apply a final (linear) classifier.
+        return self.classifier(h)
--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,16 @@
-from text.lstm import SimpleLSTM
-from text.rnn import SimpleRNN
-from text.transformer import (TransformerClassificationModel,
-                              TransformerLangModel)
-from vision.densenet import DenseNet40
-from vision.lenet import LeNet5
+try:
+    from text.lstm import SimpleLSTM
+    from text.rnn import SimpleRNN
+    from text.transformer import (TransformerClassificationModel,
+                                  TransformerLangModel)
+except BaseException:
+    pass
+try:
+    from vision.densenet import DenseNet40
+    from vision.lenet import LeNet5
+except BaseException:
+    pass
+try:
+    from graph.gcn import SimpleGCN
+except BaseException:
+    pass
