f8d7e952467fe753d7100c2748c4fd9b080a993f
duce3790 <duce3790@gmail.com>
2023-04-23 11:45:33 +0800

    add custom model

--- vision/CNNCifar.py
+++ vision/CNNCifar.py
@@ -5,20 +5,98 @@ import torch.nn.functional as F
 
 
 class CNNCifar(nn.Module):
-    def __init__(self):
+    def __init__(self, args):
         super(CNNCifar, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+
+        self.conv5 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv7 = nn.Conv2d(128, 128, 1, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU()
+
+        self.conv8 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv9 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv10 = nn.Conv2d(256, 256, 1, padding=1)
+        self.pool4 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.relu4 = nn.ReLU()
+
+        self.conv11 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv12 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv13 = nn.Conv2d(512, 512, 1, padding=1)
+        self.pool5 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.relu5 = nn.ReLU()
+
+        self.fc14 = nn.Linear(512 * 4 * 4, 1024)
+        self.drop1 = nn.Dropout()
+        self.fc15 = nn.Linear(1024, 1024)
+        self.drop2 = nn.Dropout()
+        self.fc16 = nn.Linear(1024, 10)
+
+    def forward(self, x, is_feat=False, preact=False):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.bn1(x)
+        f1_pre = x
+        x = self.relu1(x)
+        f1 = x
+
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.pool2(x)
+        x = self.bn2(x)
+        f2_pre = x
+        x = self.relu2(x)
+        f2 = x
+
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.pool3(x)
+        x = self.bn3(x)
+        f3_pre = x
+        x = self.relu3(x)
+        f3 = x
+
+        x = self.conv8(x)
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.pool4(x)
+        x = self.bn4(x)
+        f4_pre = x
+        x = self.relu4(x)
+        f4 = x
+
+        x = self.conv11(x)
+        x = self.conv12(x)
+        x = self.conv13(x)
+        x = self.pool5(x)
+        x = self.bn5(x)
+        f5_pre = x
+        x = self.relu5(x)
+        f5 = x
+        # print(" x shape ",x.size())
+        x = x.view(-1,512*4*4)
+        x = F.relu(self.fc14(x))
+        x = self.drop1(x)
+        f6_pre = x
+        x = F.relu(self.fc15(x))
+        f6 = x
+        x = self.drop2(x)
+        x = self.fc16(x)
+
         return F.log_softmax(x, dim=1)
